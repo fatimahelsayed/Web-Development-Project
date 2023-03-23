ca1502c38a8d6ac334aa49e789e130c28c838c20
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-03-23 12:48:37 +0200

    Update trial.js
    fixed glitches

--- trial.js
+++ trial.js
@@ -1,16 +1,24 @@
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.next-arrow')];
-const preBtn = [...document.querySelectorAll('.prev-arrow')];
 
-productContainers.forEach((item, i) => {
-    let containerDimenstions = item.getBoundingClientRect();
-    let containerWidth = containerDimenstions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
+function newcollectionslider() {
+    const prod_cont = [...document.querySelectorAll('.product-container')];
+    const nxtBtn = [...document.querySelectorAll('.next_arrow')];
+    const preBtn = [...document.querySelectorAll('.prev_arrow')];
+    
+    prod_cont.forEach((item, i) => {
+        let containerDimensions = item.getBoundingClientRect();
+        let containerWidth = containerDimensions.width;
+    
+            nxtBtn[i].addEventListener('click', () => {
+                item.scrollLeft += containerWidth;
+            })
+        
+       
+    
+            preBtn[i].addEventListener('click', () => {
+                item.scrollLeft -= containerWidth;
+            })
+        
+        
     })
+}
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
