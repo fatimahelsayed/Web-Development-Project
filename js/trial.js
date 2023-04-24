e4516658a03bb71a09b0d3c12c75a703a53451bd
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-04-24 03:14:37 +0200

    Update trial.js

--- js/trial.js
+++ js/trial.js
@@ -22,3 +22,24 @@ function newcollectionslider() {
     })
 }
 
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals=document.querySelectorAll('.reveal');
+    for(var i=0;i<reveals.length;i++){
+        var windowheight=window.innerHeight;
+        var revealtop=reveals[i].getBoundingClientRect().top;
+        var revealpoint=150;
+
+        if(revealtop<windowheight-revealpoint){
+            reveals[i].classList.add('active');
+        }
+       
+     }
+ }
+
+ function closesidebar() {
+    document.getElementById("sidebar").style.display = "none";
+  }
+  
+
