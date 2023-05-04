82fd12fdc0023fa59d01df843ae5dc2558856dc0
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-05-04 16:00:33 +0300

    Create orders.js

---
+++ js/orders.js
@@ -0,0 +1,16 @@
+const search = document.querySelector('.search_orders input'),
+table_rows = document.querySelectorAll('tbody tr'),
+table_headings = document.querySelectorAll('thead th');
+
+search.addEventListener('input', searchTable);
+
+function searchTable() {
+    table_rows.forEach((row, i) => {
+        let table_data = row.textContent.toLowerCase(),
+            search_data = search.value.toLowerCase();
+
+        row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
+        row.style.setProperty('--delay', i / 25 + 's');
+    });
+    
+}
