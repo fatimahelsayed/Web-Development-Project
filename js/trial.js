fd802263fd7eb8791ff1e8481230eaf9b5709ffb
fatimahelsayed <106448000+fatimahelsayed@users.noreply.github.com>
2023-05-05 16:23:28 +0300

    Update trial.js

--- js/trial.js
+++ js/trial.js
@@ -1,4 +1,13 @@
 
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
 function newcollectionslider() {
     const prod_cont = [...document.querySelectorAll('.product-container')];
     const nxtBtn = [...document.querySelectorAll('.next_arrow')];
