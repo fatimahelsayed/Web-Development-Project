6b17377ed5dddb4e8bacc6f4e21b0029a9894c00
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-05-05 13:46:35 +0300

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,3 +1,4 @@
+
 const search = document.querySelector('.search_orders input'),
 table_rows = document.querySelectorAll('tbody tr'),
 table_headings = document.querySelectorAll('thead th');
@@ -11,6 +12,25 @@ function searchTable() {
 
         row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
         row.style.setProperty('--delay', i / 25 + 's');
+    });
+    
+}
+
+function delete_product(){
+    var table = document.getElementById("product_list");
+    var rowCount = table_rows.length;
+    while(table_rows.length > 0) {
+        table.deleteRow(0);
+      }
+ }
+ 
+
+ function del() {
+    table_rows.forEach((row, i) => {
+        let table_data = row.textContent.toLowerCase();
+        while(table_rows.length > 0) {
+            table_rows.deleteRow(0);
+          }
     });
     
 }
